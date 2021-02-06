a0f8fbe75745487a493c4f17a8545c9e079ebbe2
Suryakiran Yanamadala <yskiran1@gmail.com>
2021-02-06 15:33:31 +0800

    new changes added

---
+++ CharOccurrances.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class CharOccurrances {
+
+	public static void main(String[] args) {
+		String name = "rajilsamyuktha";
+		
+		Map<Char, Integer> map = new HashMap<Char, Integer>();
+		
+		
+	}
+
+}
